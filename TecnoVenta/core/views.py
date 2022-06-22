f876db6cea10c860c0043dd7a4df8a6631d9f714
FabiSick <102421657+FabiSick@users.noreply.github.com>
2022-06-22 11:36:12 -0400

    Update views.py

--- TecnoVenta/core/views.py
+++ TecnoVenta/core/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
+from .models import Producto
 
 # Create your views here.
 
 def home(request):
+    producto= Producto.objects.all()
 
-    return render(request, 'core/contacto.html')
+    datos = {
+        'Productos': producto
+    }
+
+    return render(request, 'core/home.html', datos)
+
     
