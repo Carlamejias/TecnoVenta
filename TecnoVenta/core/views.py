141b247954d6ab794b5fb8b7a5088b6ed15dd34f
FabiSick <102421657+FabiSick@users.noreply.github.com>
2022-07-08 22:37:27 -0400

    Update views.py

--- TecnoVenta/core/views.py
+++ TecnoVenta/core/views.py
@@ -12,20 +12,29 @@ def home(request):
 
     return render(request, 'core/home.html', datos)
 
-    def form_producto(request):
-    
-    datos = {
-        'form': ProductoForm()
+def agregar_producto(request, ):
 
+    datos = {
+        'form':ProductoForm()
     }
-    if request.method== 'POST':
-        
-        formulario = ProductoForm(request.POST)
-        
-        if formulario.is_valid:
-        
-            formulario.save()
-        
-            datos['mesaje'] = "Guardados correctamente"
 
     return render(request, 'core/form.html', datos)
+
+def contacto(request, ):
+    
+    if request.method=="POST":
+        
+        miFormulario=FormularioContacto(request.POST)
+        
+        if miFormulario.is_valid():
+            infForm=miFormulario.cleaned_data
+            ##send_mail(infForm['nombre'], infForm['rut'], infForm['tel'], infForm['ciudad'], infForm['tipo'],
+            ##infForm.get('correo',''),['fabisick@gmail.com'],)
+            return render(request,"Gracias por contactarse con nosotr@s en 24 hrs habiles le responderemos.")
+
+    else:
+
+        miFormulario=FormularioContacto()
+
+    return render(request, "core/Contacto.html",{"form":miFormulario})
+
