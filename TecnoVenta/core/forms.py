f228c04d213398a78a2f0969dd8c56202d911670
FabiSick <102421657+FabiSick@users.noreply.github.com>
2022-07-08 22:35:00 -0400

    Update forms.py

--- TecnoVenta/core/forms.py
+++ TecnoVenta/core/forms.py
@@ -7,3 +7,13 @@ class ProductoForm(ModelForm):
      class Meta:
         model = Producto
         fields =['idProducto', 'marca', 'precio', 'modelo', 'categoria']
+          
+class FormularioContacto(forms.Form):
+   
+   nombre=forms.CharField()
+   correo=forms.EmailField()
+   rut=forms.IntegerField()
+   tel=forms.IntegerField()
+   ciudad=forms.CharField()
+   tipo=forms.BooleanField()
+   coment=forms.CharField()
