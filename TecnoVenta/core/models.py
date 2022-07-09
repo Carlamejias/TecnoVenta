afdee6199531d3f4bd9b69828d4b26da3bfc3e56
FabiSick <102421657+FabiSick@users.noreply.github.com>
2022-07-08 22:35:47 -0400

    Update models.py

--- TecnoVenta/core/models.py
+++ TecnoVenta/core/models.py
@@ -8,9 +8,15 @@ class Producto(models.Model):
     precio = models.IntegerField(verbose_name='Precio')
     modelo = models.CharField(max_length=30, verbose_name='Modelo Producto')
     categoria = models.CharField(max_length=30, verbose_name='Categoria')
+    imagen= models.ImageField(null=True, upload_to= 'productos' )
+
+    class Meta: 
+        verbose_name='producto'
+        verbose_name_plural='productos'
 
     def __str__(self):
 
         return self.marca
+
         
         
